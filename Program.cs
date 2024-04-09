189db9346f079a5a64ca983fe7eecf94b5f87349
SuperCoolNinja <sbpro570@gmail.com>
2024-04-09 15:45:47 +0200

    init'

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+
+namespace Properties
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            House house = new House();
+            Room room = new Room();
+            Door houseDoor = new Door();
+            Door roomDoor = new Door();
+
+            house.AddDoor(houseDoor);
+            room.AddDoor(roomDoor);
+            house.AddRoom(room);
+        }
+    }
+
+
+    public class House
+    {
+        private List<Room> _rooms;
+        private List<Door> _doors;
+
+        public List<Room> Rooms => _rooms;
+        public List<Door> Doors => _doors;
+
+        public House()
+        {
+            _rooms = new List<Room>();
+            _doors = new List<Door>();
+        }
+
+        public void AddDoor(Door door) => _doors.Add(door);
+        public void AddRoom(Room room) => _rooms.Add(room);
+    }
+
+
+    public class Room
+    {
+        private List<Door> _doors;
+        public List<Door> Door => _doors;
+
+        public Room()
+        {
+            _doors = new List<Door>();
+        }
+
+        public void AddDoor(Door door) => _doors.Add(door);
+    }
+
+
+    public class Door
+    {
+        private bool _isOpen;
+        public bool IsOpen => _isOpen;
+
+        public Door()
+        {
+            _isOpen = false;
+        }
+
+        public void Open()
+        {
+            if (_isOpen)
+                Console.WriteLine("Door already opened. Ain't done anything.");
+            else
+                _isOpen = true;
+        }
+
+        public void Close()
+        {
+            if (!_isOpen)
+                Console.WriteLine("Door already closed. Ain't done anything.");
+            else
+                _isOpen = false;
+        }
+
+    }
+}
